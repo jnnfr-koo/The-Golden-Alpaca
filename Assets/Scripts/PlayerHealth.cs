8f51235d93929bc81d15ffd059372645a86a250c
isaiah497 <68924638+isaiah497@users.noreply.github.com>
2020-09-01 23:59:22 +1200

    Fixed Unity
    - Removed GitHub Plugin
    - Started working on health feature

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+
+    public ArrayList hearts = new ArrayList();
+    public Sprite[] healthSprites = new Sprite[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 3;
+        for (int i = 0; i < maxHealth; i++)
+        {
+
+        }
+
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Plus))
+        {
+            //TODO: Increase health by 1
+        }
+        else if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            //TODO: Decrease health by 1
+        }
+    }
+}
