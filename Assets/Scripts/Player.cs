b9323cae30966a35e7523c72ff693cea5f233dee
darion <nft4407@autuni.ac.nz>
2020-09-04 11:59:50 +1200

    Fixed Conflicts
    Was able to get back to a point where the conflicts were fixed.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float maxSpeed;
-    public PlayerMovement movement;
-
-    public Rigidbody rb;
 
     void Start()
     {
-        movement = new PlayerMovement(maxSpeed);
-        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        rb.velocity = movement.CalculateMovement(
-            Input.GetAxisRaw("Vertical"),
-            Input.GetAxisRaw("Horizontal"));
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,28 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
-<<<<<<< HEAD
-    public float Speed;
-=======
 
->>>>>>> parent of 86c8d2d... Update Movement to Method
+    Rigidbody rb;
+    const float _maxspeed = 10.0f;
 
-    public PlayerMovement(float speed)
+    private void Start()
     {
-        Speed = speed;
+        rb = GetComponent<Rigidbody>();
     }
 
-    public Vector3 CalculateMovement(float verticalMovement, float horizontalMovement)
+    private void Update()
     {
-<<<<<<< HEAD
-        float x = verticalMovement * Speed;
-        float z = horizontalMovement * Speed;
-=======
->>>>>>> parent of 86c8d2d... Update Movement to Method
+        CalculateMovement();
+    }
 
-        return new Vector3(horizontalMovement, 0, verticalMovement);
+    public void CalculateMovement()
+    {
+        float verticalMovement = rb.velocity.z;
+        float horizontalMovement = rb.velocity.x;
+
+        if (Input.GetAxisRaw("Vertical") != 0.0f)
+        {
+            verticalMovement = (_maxspeed * Input.GetAxisRaw("Vertical"));
+        }
+        else
+        {
+            verticalMovement = 0.0f;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") != 0.0f)
+        {
+            horizontalMovement = (_maxspeed * Input.GetAxisRaw("Horizontal"));
+        }
+        else
+        {
+            horizontalMovement = 0.0f;
+        }
+
         rb.velocity = new Vector3(horizontalMovement, 0, verticalMovement);
     }
 }
