f010f5015ddcd1ec7d10674a473a0ed56ecb96e5
darion <nft4407@autuni.ac.nz>
2020-09-04 11:52:20 +1200

    Reverting Further to Method Creation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement
 {
+<<<<<<< HEAD
     public float Speed;
+=======
+
+>>>>>>> parent of 86c8d2d... Update Movement to Method
 
     public PlayerMovement(float speed)
     {
@@ -13,9 +17,13 @@ public class PlayerMovement
 
     public Vector3 CalculateMovement(float verticalMovement, float horizontalMovement)
     {
+<<<<<<< HEAD
         float x = verticalMovement * Speed;
         float z = horizontalMovement * Speed;
+=======
+>>>>>>> parent of 86c8d2d... Update Movement to Method
 
         return new Vector3(horizontalMovement, 0, verticalMovement);
+        rb.velocity = new Vector3(horizontalMovement, 0, verticalMovement);
     }
 }
