cae651c13aeacf2531020f27714b0dedddf3ccdc
isaiah497 <68924638+isaiah497@users.noreply.github.com>
2020-09-04 13:04:31 +1200

    Updated heart GUI system
    - Created Prefabs for Empty, Half and Full hearts
    - Made new function for handling heart instantiation

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,18 +11,15 @@ public class PlayerHealth : MonoBehaviour
     [Header("Health Stats")]
     public float maxHealth;
     public float currentHealth;
-    
+
     [Header("Heart Assets")]
     public GameObject heartStorage;
-    public GameObject heartPrefab;
     [Space]
-    public Sprite emptyHeart;
-    public Sprite halfHeart;
-    public Sprite fullHeart;
+    public GameObject emptyHeart;
+    public GameObject halfHeart;
+    public GameObject fullHeart;
 
-    [SerializeField]
-    private ArrayList hearts = new ArrayList();
-    //private float healthIncrement = 0.5f;
+    private float healthIncrement = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -31,21 +28,57 @@ public class PlayerHealth : MonoBehaviour
 
         for (int i = 0; i < maxHealth; i++)
         {
-            hearts.Add(Instantiate(heartPrefab, heartStorage.transform));
+            Instantiate(emptyHeart, heartStorage.transform);
         }
+
         currentHealth = maxHealth;
+        updateHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Plus)) //Increase health by one half
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //Increase health by one half
         {
-            currentHealth += 0.5f;
+            currentHealth += healthIncrement;
+            updateHealth();
         }
-        else if (Input.GetKeyDown(KeyCode.Minus)) //Decrease health by one half
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) //Decrease health by one half
         {
-            currentHealth -= 0.5f;
+            currentHealth -= healthIncrement;
+            updateHealth();
+        }
+    }
+
+    private void updateHealth()
+    {
+        if (currentHealth > maxHealth) //Check if health goes over max health
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            foreach (Transform heart in transform)
+            {
+                Destroy(heart.gameObject);
+            }
+
+            //TODO: Allow for multiple empty hearts
+            for (int i = 0; i < maxHealth; i++)
+            {
+                if ((currentHealth > i) && (currentHealth < i + 1))
+                {
+                    Instantiate(halfHeart, heartStorage.transform);
+                }
+                else if (currentHealth == i)
+                {
+                    Instantiate(emptyHeart, heartStorage.transform);
+                }
+                else
+                {
+                    Instantiate(fullHeart, heartStorage.transform);
+                }
+            }
         }
     }
 }
