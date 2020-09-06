76fcbd25986c2a542224adbc7956372153442ef2
darion <nft4407@autuni.ac.nz>
2020-09-07 11:36:56 +1200

    Added Comments to Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Class to control player movement throughout levels.
+ */
 public class PlayerMovement : MonoBehaviour
 {
-
+    /*
+     * movement: Holds new input direction from user to move to.
+     */
     public Rigidbody rb;
-    public float _maxspeed;
+    public float maxspeed;
     public Vector3 movement;
 
     private void Start()
@@ -16,16 +21,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // Get player input for new direction, and store it.
         movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
 
     private void FixedUpdate()
     {
+        // Call method to add new vector to the speed of the player.
         rb.velocity = CalculateMovement(movement);
     }
 
     public Vector3 CalculateMovement(Vector3 direction)
     {   
-        return direction * _maxspeed;
+        return direction * maxspeed;
     }
 }
