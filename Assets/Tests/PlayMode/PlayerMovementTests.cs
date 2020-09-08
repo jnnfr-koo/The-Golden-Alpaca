78909711f68154f922706ab1d2636ba73e4c4258
darion <nft4407@autuni.ac.nz>
2020-09-08 16:35:55 +1200

    Commented Out UnityTest for Movement
    The UnityTest would reqiure some extra files, which must first be checked off

--- Assets/Tests/PlayMode/PlayerMovementTests.cs
+++ Assets/Tests/PlayMode/PlayerMovementTests.cs
@@ -8,13 +8,15 @@ namespace Tests
 {
     public class PlayerMovementTests
     {
-        [UnityTest]
-        public IEnumerator Movement_Along_X_Axis_For_Horizontal_Input()
-        {
-            GameObject toAddPlayer = new GameObject();
-            var player = toAddPlayer.AddComponent<Player>();
+        //[UnityTest]
+        //public IEnumerator Movement_Along_X_Axis_For_Horizontal_Input()
+        //{
+        //    GameObject toAddPlayer = new GameObject();
+        //    var player = toAddPlayer.AddComponent<Player>();
 
-            yield return null;
-        }
+        //    player.maxspeed = 10.0f;
+
+        //    yield return null;
+        //}
     }
 }
